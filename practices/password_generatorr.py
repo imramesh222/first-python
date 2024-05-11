edcd070e86abcb34f50409b22216b22b2e5ec225
imramesh222 <imrameshrawat@gmail.com>
2024-05-12 00:26:05 +0545

    Changes made in project

--- practices/password_generatorr.py
+++ practices/password_generatorr.py
@@ -1,36 +1,44 @@
 import random
-letters = []
-
-for ascii_value in range(ord('A'), ord('Z')+1):
-    letters.append(chr(ascii_value))
-
-for ascii_value in range(ord('a'), ord('z')+1):
-    letters.append(chr(ascii_value))
-
-print(letters)
-
+letters=["a", "b", "c", "d", "e","f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+numbers = ['0','1','2','3','4','5','6','7','8','9']
 symbols=['!','<','>','@','%','*','^']
-print(symbols)
+
+print("Welcome to the PyPassword Generator!")
+no_letters=int(input("Enter no of letters you like to use in your password \n"))
+no_symbols=int(input("Enter no of symbols you want to use in your password \n"))
+no_number=int(input("Enter no of numbers you want to use in your password \n"))
 
 
-for numbers in range(10):
-    print(numbers, end=' ')
+# # easy steps
+# password=""
+# for char in range(1,no_letters+1):
+#   password+=random.choice(letters)
 
+# for char in range(1,no_symbols+1):
+#   password+=random.choice(symbols)
 
-print("Welcome to password generator! ")
-no_numbers=int(input("Enter how many numbers do you want to add to your password"))
-no_letters=int(input("Enter how many letters to add to your password"))
-no_symbols=int(input("Enter how many symbols to add to your password"))
+# for char in range(1,no_number+1):
+#   password+=random.choice(numbers)
+
+# print(password)
+
+# hard steps
 
 password_list=[]
+for char in range(1,no_letters+1):
+  password_list+=random.choice(letters)
 
-for char in range(0,len(no_numbers+1)):
-      password_list+=random.choice(numbers)
+for char in range(1,no_symbols+1):
+  password_list+=random.choice(symbols)
 
-for char in range(0,len(no_letters+1)):
-     password_list+=random.choice(letters)
-
-for char in range(0,len(no_symbols+1)):
-     password_list+=random.choice(symbols)
+for char in range(1,no_number+1):
+  password_list+=random.choice(numbers)
 
 print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password=""
+for char in password_list:
+  password+=char
+print(f"your password is {password}")  
