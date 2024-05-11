2dda215748ba47c6f6d7bad450f3406cad4ff475
imramesh222 <imrameshrawat@gmail.com>
2024-05-11 15:29:18 +0545

    add 5th day

---
+++ 5th-day/fizz_buzz.py
@@ -0,0 +1,9 @@
+for number in range(0,100):
+  if number%3==0 and number%5==0:
+    print("FizzBuzz")
+  elif number%5==0:
+    print("Buzz")
+  elif number%5==0:
+    print("Fizz")
+  else:
+    print(number)
