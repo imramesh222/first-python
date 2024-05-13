137fb55d6ff456e1edcce11424f72cec684037df
imramesh222 <imrameshrawat@gmail.com>
2024-05-14 00:19:04 +0545

    practise of SPR

--- sissors_paper.py
+++ sissors_paper.py
@@ -20,4 +20,30 @@ else:
   print("Invalid choice")
   exit()
 
-computerChoice=random.choice()
+computerChoice=random.randint(0,2)
+
+if computerChoice == 0:
+  computerChoice = rock
+  print(f"Computer chooses {computerChoice}")
+
+elif computerChoice == 1:
+  computerChoice = paper
+  print(f"Computer chooses {computerChoice}")
+
+elif computerChoice == 2:
+  computerChoice = scissors
+  print(f"Computer chooses {computerChoice}")
+
+if userChoice==computerChoice:
+  print("Draw")
+elif userChoice==1 and computerChoice==0:
+  print("You win")
+
+elif userChoice==2 and computerChoice==1:
+  print("You win")
+
+elif userChoice==0 and computerChoice==2:
+  print("You win")
+
+else:
+  print("You lose")
