f23698dac25684e09e57e9450e5029669cbe95ad
imramesh222 <imrameshrawat@gmail.com>
2024-05-14 23:15:37 +0545

    create own function using define()

--- 6th-day/functions.py
+++ 6th-day/functions.py
@@ -1,3 +1,11 @@
-print("hello")
-num_char=len("hello")
-print(num_char)
+# print("hello")
+# num_char=len("hello")
+# print(num_char)
+
+#create or defining own function
+
+def my_function():
+  print("hello")
+  print("Bye")
+
+my_function()
