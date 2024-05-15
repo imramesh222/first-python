d9c97a20fe1462a7a0cf028e71d002e9dbc2de38
imramesh222 <imrameshrawat@gmail.com>
2024-05-15 23:23:20 +0545

    def

--- 6th-day/functions.py
+++ 6th-day/functions.py
@@ -10,11 +10,27 @@
 
 # my_function()
 
-def my_function():
-  for n in range(6)
-  print(n)
+# "Write a Python function called calculate_average that takes a list of numbers as input and returns the average (mean) of those numbers. Then, write a program that prompts the user to enter a list of numbers separated by spaces, calls the calculate_average function, and prints the resulting average."
 
-for add in range(6):
-  add+=1
+def calculate_average(numbers):
+    """
+    Function to calculate the average of a list of numbers.
+    
+    Parameters:
+        numbers (list of float): List of numbers to calculate the average from.
+    
+    Returns:
+        float: The average of the numbers.
+    """
+    if len(numbers) == 0:
+        return 0
+    else:
+        return sum(numbers) / len(numbers)
 
-my_function()
+# Prompting the user for input
+input_numbers = input("Enter a list of numbers separated by spaces: ")
+numbers_list = [float(num) for num in input_numbers.split()]
+
+# Calling the calculate_average function and printing the result
+average = calculate_average(numbers_list)
+print("The average of the numbers is:", average)
