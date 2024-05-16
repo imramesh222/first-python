fed61714dab66c381c8ddaee235784c11801fa83
imramesh222 <imrameshrawat@gmail.com>
2024-05-16 23:55:50 +0545

    practise

--- practices/2nd-day.py
+++ practices/2nd-day.py
@@ -19,3 +19,11 @@ print(tip_per_person)
 # per_person_tip=float(tip)/int(no_of_person)
 # print(per_person_tip)
 
+print("Welcome to the Tip calculator")
+bill=input("What was the total bill amount?\n")
+percent=input("What percent tip of the total bill amount will you like to give?\n")
+no_of_person=input("How many people to spilt the bill?\n")
+
+tip=float(bill)*float(percent)/100
+per_person_tip=float(tip)/int(no_of_person)
+print(per_person_tip)
