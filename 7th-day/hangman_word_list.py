d82cfc377f5aa10220d742ab2b3307f43e367e82
imramesh222 <imrameshrawat@gmail.com>
2024-05-19 01:08:12 +0545

    final hangman word_list module

---
+++ 7th-day/hangman_word_list.py
@@ -0,0 +1,10 @@
+# hangman_word_list.py
+
+word_list = [
+    "apple", "banana", "mango", "grapes", "orange", "watermelon", "pineapple", "papaya", "guava",
+    "strawberry", "cherry", "blueberry", "blackberry", "raspberry", "lemon", "lime", "coconut",
+    "avocado", "pomegranate", "peach", "pear", "plum", "apricot", "kiwi", "nectarine", "fig",
+    "grapefruit", "tangerine", "date", "passionfruit", "dragonfruit", "lychee", "persimmon",
+    "mandarin", "quince", "starfruit", "jackfruit", "durian", "rambutan", "mulberry", "cranberry",
+    "boysenberry", "gooseberry", "elderberry", "currant", "jambul", "olive", "cucumber", "tomato"
+]
