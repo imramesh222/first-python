21184a3994f1c095261d6e600f976fe670c59d23
imramesh222 <imrameshrawat@gmail.com>
2024-05-18 08:54:53 +0545

    added files in day 7

--- 7th-day/hangman.py
+++ 7th-day/hangman.py
@@ -1,3 +1,12 @@
-list = ['larry', 'curly', 'moe']
-if 'moe' in list:
-    print('yes') 
+# list = ['larry', 'curly', 'moe']
+# if 'moe' in list:
+#     print('yes') 
+import random
+print("Wewlcome to Hangman words game\n")
+
+words_list = ["apple", "banana", "mango", "grapes", "orange", "watermelon", "pineapple", "papaya", "guava", "strawberry", "cherry", "blueberry", "blackberry", "raspberry", "lemon", "lime", "coconut", "avocado", "pomegranate", "peach", "pear", "plum", "apricot"]
+random_word=random.choice(words_list)
+
+def generate_blanks(random_word):
+  word=random_word.split()
+  
