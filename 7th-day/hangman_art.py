073501633ef6ea5206d4d14022af6cd94035a7d5
imramesh222 <imrameshrawat@gmail.com>
2024-05-19 01:24:05 +0545

    hangman_word_list module imported

--- 7th-day/hangman_art.py
+++ 7th-day/hangman_art.py
@@ -48,3 +48,24 @@ HANGMANPICS = ['''
  / \  |
       |
 =========''']
+
+hangman_logo=['''88                                                                            
+88                                                                            
+88                                                                            
+88,dPPYba,  ,adPPYYba, 8b,dPPYba,   ,adPPYb,d8 88,dPYba,,adPYba,  ,adPPYYba,  
+88P'    "8a ""     `Y8 88P'   `"8a a8"    `Y88 88P'   "88"    "8a ""     `Y8  
+88       88 ,adPPPPP88 88       88 8b       88 88      88      88 ,adPPPPP88  
+88       88 88,    ,88 88       88 "8a,   ,d88 88      88      88 88,    ,88  
+88       88 `"8bbdP"Y8 88       88  `"YbbdP"Y8 88      88      88 `"8bbdP"Y8  
+                                    aa,    ,88                                
+                                     "Y8bbdP"                                 
+             
+             
+             
+             
+8b,dPPYba,   
+88P'   `"8a  
+88       88  
+88       88  
+88       88  
+             ''']
